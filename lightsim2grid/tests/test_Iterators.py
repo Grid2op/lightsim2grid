fixing the units for powerline and transformers in the iterators

lightsim2grid/tests/test_Iterators.py
@@ -72,7 +72,7 @@ class MakeTests(unittest.TestCase):
         assert el.has_res, f"trafo {trafo_id} don't have any results"
         assert np.abs(el.res_p_hv_mw - p_or[trafo_id]) <= tol, f"trafo {trafo_id} has wrong p_hv"
         assert np.abs(el.res_q_hv_mvar - q_or[trafo_id]) <= tol, f"trafo {trafo_id} has wrong q_hv"
-        assert np.abs(el.res_a_hv_a - 0.001 * a_or[trafo_id]) <= tol, f"trafo {trafo_id} has wrong a_hv"
+        assert np.abs(el.res_a_hv_ka - 0.001 * a_or[trafo_id]) <= tol, f"trafo {trafo_id} has wrong a_hv"
 
     def test_getters_trafo(self):
         """test that the trafo getter return the right values"""
@@ -110,7 +110,7 @@ class MakeTests(unittest.TestCase):
         assert el.has_res, f"line {line_id} don't have any results"
         assert np.abs(el.res_p_or_mw - p_or[line_id]) <= tol, f"line {line_id} has wrong p_or"
         assert np.abs(el.res_q_or_mvar - q_or[line_id]) <= tol, f"line {line_id} has wrong q_or"
-        assert np.abs(el.res_a_or_a - 0.001 * a_or[line_id]) <= tol, f"line {line_id} has wrong a_or"
+        assert np.abs(el.res_a_or_ka - 0.001 * a_or[line_id]) <= tol, f"line {line_id} has wrong a_or"
 
     def test_getters_line(self):
         """test that the line getter return the right values"""
@@ -132,10 +132,10 @@ class MakeTests(unittest.TestCase):
         gen_info = data_lines[0]
         self.aux_line_ok(gen_info, 0, tol, data_ref)
         with self.assertRaises(ValueError):
-            gen_info = data_lines[-1]
+            line_info = data_lines[-1]
 
         with self.assertRaises(ValueError):
-            gen_info = data_lines[nb_line]
+            line_info = data_lines[nb_line]
 
 
 if __name__ == "__main__":