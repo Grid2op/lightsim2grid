5a41d20bc8641e374ad55d8c7fb0e6d17031a45f
BDonnot <benjamin.donnot@gmail.com>
2021-07-26 16:00:45 +0200

    ready for release of version 0.5.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, RTE France'
 author = 'Benjamin DONNOT'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.5.2'
 version = '0.5'
 
 # -- General configuration ---------------------------------------------------
