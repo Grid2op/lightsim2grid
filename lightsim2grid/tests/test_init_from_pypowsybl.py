330ee248ee9f183cb87fb3cb7b82c67dbe4e0195
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2023-09-15 09:53:50 +0200

    adding more tests for pypowsybl integration [skip ci]

--- lightsim2grid/tests/test_init_from_pypowsybl.py
+++ lightsim2grid/tests/test_init_from_pypowsybl.py
@@ -41,9 +41,6 @@ class AuxInitFromPyPowSyBl:
     
     def setUp(self) -> None:
         self.network = self.get_pypo_grid()
-        # self.network = pp.network.create_ieee118()
-        
-        # modify the grid for the 2 weird powerlines TODO
         self.fix_grid_pypow()
         
         # init equivalent pandapower grid (if any)
@@ -53,7 +50,7 @@ class AuxInitFromPyPowSyBl:
             self.ref_samecase = init_from_pp(self.pp_samecase)
             self.can_pp = True
         else:
-            # TODO will crash
+            # TODO will crash later if no equiv grid
             self.can_pp = False
             self.pp_samecase = None
             self.ref_samecase = None
@@ -86,8 +83,40 @@ class AuxInitFromPyPowSyBl:
         v_ls = self.gridmodel.dc_pf(self.V_init_dc, 2, self.tol)
         v_ls_ref = self.ref_samecase.dc_pf(self.V_init_dc, 2, self.tol)
         slack_id = self.get_slackbus_id()
+        
+        bus_or = [67]
+        bus_ex = [115]
+        l_id_ls = [el.id for el in self.gridmodel.get_lines() if (el.bus_or_id in bus_or) and (el.bus_ex_id in bus_ex) ]
+        self.gridmodel.get_lines()[173].r_pu
+        self.gridmodel.get_lines()[173].x_pu
+        self.gridmodel.get_lines()[173].h_pu
+        
+        bus_or = [85]
+        bus_ex = [86]
+        l_id_ls = [el.id for el in self.gridmodel.get_lines() if (el.bus_or_id in bus_or) and (el.bus_ex_id in bus_ex) ]
+        self.gridmodel.get_lines()[l_id_ls[0]].r_pu
+        self.gridmodel.get_lines()[l_id_ls[0]].x_pu
+        self.gridmodel.get_lines()[l_id_ls[0]].h_pu
+
+        bus_or = [64]
+        bus_ex = [67]
+        l_id_ls = [el.id for el in self.gridmodel.get_lines() if (el.bus_or_id in bus_or) and (el.bus_ex_id in bus_ex) ]
+        self.gridmodel.get_lines()[l_id_ls[0]].r_pu
+        self.gridmodel.get_lines()[l_id_ls[0]].x_pu
+        self.gridmodel.get_lines()[l_id_ls[0]].h_pu
+        
+        l_id_ls_ref = [el.id for el in self.ref_samecase.get_lines() if (el.bus_or_id in bus_or) and (el.bus_ex_id in bus_ex) ]
+        l_id_ls_ref = [el.id for el in self.ref_samecase.get_trafos() if (el.bus_hv_id in bus_or) and (el.bus_lv_id in bus_ex) ]
+        self.ref_samecase.get_lines()[l_id_ls_ref[0]].r_pu
+        self.ref_samecase.get_lines()[l_id_ls_ref[0]].x_pu
+        self.ref_samecase.get_lines()[l_id_ls_ref[0]].h_pu
+                        
+                        
+        import pdb
+        pdb.set_trace()
         assert np.abs(v_ls - v_ls_ref).max() <= self.tol_eq, "error for vresults for dc"
-        assert np.abs(self.gridmodel.get_dcYbus() - self.ref_samecase.get_dcYbus()).max() <= self.tol_eq, "error for dcYbus"
+        tmp_ = self.gridmodel.get_dcYbus() - self.ref_samecase.get_dcYbus()
+        assert np.abs(tmp_).max() <= self.tol_eq, "error for dcYbus"
         # check Sbus without slack
         if slack_id > 0:
             assert np.abs(self.gridmodel.get_Sbus()[:slack_id] - self.ref_samecase.get_Sbus()[:slack_id]).max() <= self.tol_eq, "error for dc Sbus"
@@ -162,7 +191,17 @@ class TestCase118FromPypo(AuxInitFromPyPowSyBl, unittest.TestCase):
     def get_equiv_pdp_grid(self):
         return pn.case118()
     def fix_grid_pypow(self):
-        pass
+        vn = np.array([161.0, 138.0])
+        to_pu = vn*vn*0.01
+        self.network.update_lines(id=["L86-87-1", "L68-116-1"],
+                                  r=np.array([0.02828 , 0.00034]) * to_pu,
+                                  x=np.array([0.2074 , 0.00405]) * to_pu,
+                                  b1=np.array([0.0445, 0.164]) / 2. / to_pu,
+                                  b2=np.array([0.0445, 0.164]) / 2. / to_pu,
+                                  g1=np.array([0., 0.]),
+                                  g2=np.array([0., 0.]),
+        )
+        lf.run_ac(self.network)    
     def get_gen_slack_id(self):
         return 29
     def get_slackbus_id(self):
