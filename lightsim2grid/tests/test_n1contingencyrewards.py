b9a0eb592a07bf966a5846e522850282e468ebc0
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2024-07-12 11:47:17 +0200

    trying to fix the test of n1 contingency reward on CI

--- lightsim2grid/tests/test_n1contingencyrewards.py
+++ lightsim2grid/tests/test_n1contingencyrewards.py
@@ -88,8 +88,13 @@ class TestN1ContingencyReward_Base(unittest.TestCase):
         if self.my_ids is None:
             self.my_ids = list(range(type(self.env).n_line))
         self.env.observation_space.change_reward(EpisodeDurationReward)
+        self.aux_reset_correctly()
         return super().setUp()
     
+    def aux_reset_correctly(self):
+        obs = self.env.reset(seed=0, options={"time serie id": 0})
+        return obs
+        
     def tearDown(self) -> None:
         self.env.close()
         return super().tearDown()
@@ -124,14 +129,14 @@ class TestN1ContingencyReward_Base(unittest.TestCase):
         assert reward == (len(self.my_ids) - unsafe_cont), f"{reward} vs {(len(self.my_ids) - unsafe_cont)}"
         
     def test_do_nothing(self):
-        obs = self.env.reset()
+        obs = self.aux_reset_correctly()
         obs, reward, done, info = self.env.step(self.env.action_space())
         self._aux_test_reward(obs, reward)
         obs, reward, done, info = self.env.step(self.env.action_space())
         self._aux_test_reward(obs, reward)
         
     def test_disconnected_line(self):
-        obs = self.env.reset()
+        obs = self.aux_reset_correctly()
         obs, reward, done, info = self.env.step(self.env.action_space(
             {"set_line_status": [(0, -1)]}))
         assert not done
@@ -148,7 +153,7 @@ class TestN1ContingencyReward_Base(unittest.TestCase):
         self._aux_test_reward(obs, reward)
         
     def test_modif_topo(self):
-        obs = self.env.reset()
+        obs = self.aux_reset_correctly()
         obs, reward, done, info = self.env.step(self.env.action_space(
             {"set_bus": {"substations_id": [(1, (1, 2, 1, 2, 1, 2))]}}))
         assert not done
@@ -167,7 +172,7 @@ class TestN1ContingencyReward_Base(unittest.TestCase):
         self._aux_test_reward(obs, reward)
     
     def test_copy(self):
-        obs = self.env.reset()
+        obs = self.aux_reset_correctly()
         env_cpy = self.env.copy()
         obs_cpy = env_cpy.copy()
         assert self.env._reward_helper.template_reward._backend is not env_cpy._reward_helper.template_reward._backend
