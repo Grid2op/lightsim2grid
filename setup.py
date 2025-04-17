be4b3e42d303fb96cc0ef103cd420f1bd64a3a38
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2025-04-17 17:15:30 +0200

    update for version info
    Signed-off-by: DONNOT Benjamin <benjamin.donnot@rte-france.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import warnings
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 
-__version__ = "0.10.3.dev0"
+__version__ = "0.10.3"
 KLU_SOLVER_AVAILABLE = False
 
 # Try to link against SuiteSparse (if available)
