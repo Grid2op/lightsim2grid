find a bug in the computation of gqmin per bus
Signed-off-by: DONNOT Benjamin <benjamin.donnot@rte-france.com>

lightsim2grid/tests/test_pickleable.py
@@ -45,25 +45,40 @@ class TestPickle(unittest.TestCase):
                     assert np.allclose(getattr(line1, attr_nm), getattr(line2, attr_nm)), f"{method_name} error for {attr_nm}: {getattr(line1, attr_nm)} vs {getattr(line2, attr_nm)}"
                 
             
-    def _aux_test_1side(self, grid1, grid2, method_name, test_results=False):
-        assert len(getattr(grid1, method_name)()) == len(getattr(grid2, method_name)())
-        for line1, line2 in zip(getattr(grid1, method_name)(), getattr(grid2, method_name)()):
-            for attr_nm in ["id", "name", "sub_id",
+    def _aux_test_1side(self, grid1, grid2, method_name, test_results=False,
+                        add_attr_int=None,
+                        add_attr_float=None):
+        li_attr_to_test_int = ["id", "name", "sub_id",
                             "pos_topo_vect", 
                             "connected",
                             "bus_id",
-                            ]:
+                            ]
+        if add_attr_int is not None:
+            li_attr_to_test_int += add_attr_int
+            
+        li_attr_to_test_float = []
+        if add_attr_float is not None:
+            li_attr_to_test_float += add_attr_float
+        assert len(getattr(grid1, method_name)()) == len(getattr(grid2, method_name)())
+        for line1, line2 in zip(getattr(grid1, method_name)(), getattr(grid2, method_name)()):
+            for attr_nm in li_attr_to_test_int:
                 assert getattr(line1, attr_nm) == getattr(line2, attr_nm), f"{method_name} error for {attr_nm}: {getattr(line1, attr_nm)} vs {getattr(line2, attr_nm)}"
             
-            for attr_nm in []:
+            for attr_nm in li_attr_to_test_float:
                 # TODO other attributes
                 assert np.allclose(getattr(line1, attr_nm), getattr(line2, attr_nm)), f"{method_name} error for {attr_nm}: {getattr(line1, attr_nm)} vs {getattr(line2, attr_nm)}"
             
             # TODO add the "results" "dataframe"
-            if test_results:
-                for attr_nm in ["res_p_mw", "res_q_mvar", "res_theta_deg", "res_v_kv"]:
-                    # TODO other attributes
-                    assert np.allclose(getattr(line1, attr_nm), getattr(line2, attr_nm)), f"{method_name} error for {attr_nm}: {getattr(line1, attr_nm)} vs {getattr(line2, attr_nm)}"
+            if not test_results:
+                continue
+            li_attr_to_test = ["res_p_mw", "res_q_mvar", "res_theta_deg", "res_v_kv"]
+            for attr_nm in li_attr_to_test:
+                # TODO other attributes
+                if not np.allclose(getattr(line1, attr_nm), getattr(line2, attr_nm)):
+                    diff_ = np.abs(getattr(line1, attr_nm) - getattr(line2, attr_nm))
+                    
+                    raise AssertionError(f"{method_name} error for {attr_nm} for gen id {line1.id} ({line1.name}): {getattr(line1, attr_nm)} "
+                                            f"vs {getattr(line2, attr_nm)} -> {diff_}")
                 
         
     def aux_test_2sides(self, grid1, grid2, test_results=False):
@@ -72,7 +87,9 @@ class TestPickle(unittest.TestCase):
         
     def aux_test_1side(self, grid1, grid2, test_results=False):
         self._aux_test_1side(grid1, grid2, "get_loads", test_results)
-        self._aux_test_1side(grid1, grid2, "get_generators", test_results)
+        self._aux_test_1side(grid1, grid2, "get_generators", test_results,
+                             add_attr_int=["is_slack", "voltage_regulator_on"],
+                             add_attr_float=["slack_weight", "target_vm_pu", "min_q_mvar", "max_q_mvar"])
         self._aux_test_1side(grid1, grid2, "get_storages", test_results)
         self._aux_test_1side(grid1, grid2, "get_shunts", test_results)
                 
@@ -97,9 +114,8 @@ class TestPickle(unittest.TestCase):
 
                 # test dc_pf
                 V_0 = np.ones(nb_bus_total, dtype=complex)
+                V_1 = V_0.copy()
                 V_0 = self.env.backend._grid.dc_pf(V_0, max_it, tol)
-
-                V_1 = np.ones(nb_bus_total, dtype=complex)
                 V_1 = backend_1._grid.dc_pf(V_1, max_it, tol)
 
                 assert np.all(np.abs(V_0 - V_1) <= 1e-7), "dc pf does not lead to same results"