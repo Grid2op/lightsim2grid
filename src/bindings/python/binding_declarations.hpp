bc05a7cc920b7dd3ad5b471854ff9e046c55c88a
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2026-04-23 10:23:58 +0200

    fix CI issues on github
    Signed-off-by: DONNOT Benjamin <benjamin.donnot@rte-france.com>

--- src/bindings/python/binding_declarations.hpp
+++ src/bindings/python/binding_declarations.hpp
@@ -10,6 +10,9 @@
 #define BINDING_DECLARATIONS_HPP
 
 #include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <pybind11/complex.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 
